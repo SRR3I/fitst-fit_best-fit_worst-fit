7bfa65be7b1a6e8fc5d6a0cea665dda770048da3
srr3i <salhsari8@gmail.com>
2024-03-24 16:14:25 +0300

    create 2 function about best and worst

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 def first_fit(partitions, processes):
+    print('\nFirst-fit')
     i = 0
     MustWait = None
     while i < len(processes):
@@ -6,23 +7,65 @@ def first_fit(partitions, processes):
         for j in range(len(partitions)):
             if i < len(processes):
                 if processes[i] <= partitions[j]:
-                    print(f"{processes[i]} is put in {partitions[j]}")
+                    print(f"{processes[i]} is put in {partitions[j]} partition")
                     partitions[j] -= processes[i]
                     complete[i] = False
                     i += 1
                 else:
                     MustWait = processes[i]
         if all(complete):
-            print(MustWait, 'must want')
+            print(MustWait, 'must wait')
             break
 
-
 def best_fit(partitions, processes):
-    return
+    print('\nBest-fit')
+    MustWait = None
+    for i in range(len(processes)):
+        partitions.sort()
+        complete = [True]*len(processes)
+        for j in range(len(partitions)):
+            if i < len(processes):
+                if processes[i] <= partitions[j]:
+                    print(f"{processes[i]} is put in {partitions[j]} partition")
+                    partitions[j] -= processes[i]
+                    complete[i] = False
+                    break
+                else:
+                    MustWait = processes[i]
+        if MustWait != None and all(complete):
+            print(MustWait, 'must wait')
 
 
 def worst_fit(partitions, processes):
-    return
+    print('\nWorst-fit')
+    MustWait = None
+    for i in range(len(processes)):
+        partitions.sort()
+        partitions.reverse()
+        complete = [True]*len(processes)
+        for j in range(len(partitions)):
+            if i < len(processes):
+                if processes[i] <= partitions[j]:
+                    print(f"{processes[i]} is put in {partitions[j]} partition")
+                    partitions[j] -= processes[i]
+                    complete[i] = False
+                    break
+                else:
+                    MustWait = processes[i]
+        if MustWait != None and all(complete):
+            print(MustWait, 'must wait')
 
 
-f = first_fit([100, 500,  200, 300, 600], [212, 417, 112, 426])
+partition = [100, 500,  200, 300, 600]
+process = [212, 417, 112, 426]
+first_fit(partition, process)
+
+
+partition = [100, 500,  200, 300, 600]
+process = [212, 417, 112, 426]
+best_fit(partition, process)
+
+
+partition = [100, 500,  200, 300, 600]
+process = [212, 417, 112, 426]
+worst_fit(partition, process)
